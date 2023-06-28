73a25c217da484d66c81770354c9886ea4f0f277
Facundo Pareja <facundopareja@gmail.com>
2023-06-27 21:30:37 -0300

    fmt fix

--- users/crud.py
+++ users/crud.py
@@ -179,7 +179,7 @@ def follow_new_user(session: Session, user_id: int, _id: int):
     """Add new followed user to specified user."""
     if session.query(FollowedUsers).filter(FollowedUsers.id == user_id,
                                            FollowedUsers.followed_id == _id) \
-        .first() is not None:
+            .first() is not None:
         return get_users_followed_by(session, user_id)
     new_follow = FollowedUsers(id=user_id, followed_id=_id)
     session.add(new_follow)
